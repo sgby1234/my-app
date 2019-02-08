5983feb8c5f5eda0ee045be1d8147560c39b0e85
Shifra Goldstone <sgoldsto2@student.touro.edu>
2019-02-07 22:02:43 -0500

    Displays list

--- src/App.js
+++ src/App.js
@@ -4,25 +4,19 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	  var cards = ["Card #1", "Card #2", "Card #3", "Card #4", "Card #5"];
+	  return (
+	  <div className="App">
+	     <ul>{
+				cards.map(function(card){
+						return <li>{card}</li>;
+		})
+		 }
+		 </ul>
+		 </div>
+	  )
+	}
+
 }
 
 export default App;
